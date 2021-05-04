7f671d8824af5a1b578be24194ac9689d9a056ac
HalDoggy <fw190.quantum@gmail.com>
2021-05-04 14:21:34 +0900

    #7-trial : add trial project and install EF and SQLite

---
+++ src/DatabaseTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
